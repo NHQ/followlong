998fcdbe17e82d4d7d5a177d1d1d7b1add7ecd79
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-10 15:12:06 -0800

    monday

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -24,7 +24,7 @@ $('#filter a').click(function(){
 
 
 $(window).load(function(){
-	var jqxhr = $.ajax({ url: "/frontpage" })
+	var jqxhr = $.ajax({ url: "./frontpage" })
 	    .success(function() { alert("success"); })
 	    .error(function() { alert("error: "+errorThrown); })
 	    .complete(function() { alert("complete"); });
