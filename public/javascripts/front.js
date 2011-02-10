70c0f9650e281b9edb248fddc7f3488289cda25f
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-10 14:42:04 -0800

    monday

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -16,18 +16,23 @@ $('#filter a').click(function(){
   var selector = $(this).attr('data-filter');
   $('#container').isotope({ filter: selector });
   return false;
+
+
 });
+
+alert('go');
+$.get('/frontpage', function(data){
+	var $append;
+	for (d in data)
+	{
+		append += '<div class="articles "+'d.channel+', data-category='+d.channel+', data-score='+d.score+', id='+d.furl+'><h2><a href='d.link+', class="title">'+d.title+'</a></h2></div>'
+	}
+});
+	alert($append);
+	$('#container').isotope('insert', $append)
+
 });
 
 $(document).ready(function(){
-		alert('go');
-		$.get('/frontpage', function(data){
-			var $append;
-			for (d in data)
-			{
-				append += '<div class="articles "+'d.channel+', data-category='+d.channel+', data-score='+d.score+', id='+d.furl+'><h2><a href='d.link+', class="title">'+d.title+'</a></h2></div>'
-			}
-		});
-			alert($append);
-			$('#container').isotope('insert', $append)	
+
 });
