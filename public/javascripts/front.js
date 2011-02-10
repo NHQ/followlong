956e9f114bb48e2bf046958bfd0effc235930fd1
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-10 15:00:51 -0800

    monday

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -24,5 +24,13 @@ $('#filter a').click(function(){
 
 
 $(window).load(function(){
-	alert('go');
+	$.get('frontpage', function(data){
+		var $append;
+		for (d in data)
+		{
+			append += '<div class="articles "+'d.channel+', data-category='+d.channel+', data-score='+d.score+', id='+d.furl+'><h2><a href='d.link+', class="title">'+d.title+'</a></h2></div>'
+		}
+		alert($append);
+		$container.isotope('insert', $append)
+	})
 });
