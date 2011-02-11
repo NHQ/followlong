c32e609a75f3254c8be1f5ae488dbc89d4a1b932
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-11 17:00:30 -0600

    for no.de

--- server.js
+++ server.js
@@ -1,36 +1,114 @@
 /**
- * Important note: this application is not suitable for benchmarks!
+ * Module dependencies.
  */
 
-var http = require('http')
-  , url = require('url')
-  , fs = require('fs')
-  , io = require('../')
-  , sys = require(process.binding('natives').util ? 'util' : 'sys')
-  , server;
-    
-server = http.createServer(function(req, res){
-  // your normal server code
-  var path = url.parse(req.url).pathname;
-  switch (path){
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('<h1>Welcome. Try the <a href="/chat.html">chat</a> example.</h1>');
-      res.end();
-      break;
-      
-    case '/json.js':
-    case '/chat.html':
+var express = require('express');
+
+var app = module.exports = express.createServer(),
+    redis = require("./redis"),
+    client = redis.createClient(),
+    sub = redis.createClient(),
+    pub = redis.createClient(),
+    crypto = require('crypto')
+    , http = require('http')
+    , url = require('url')
+    , fs = require('fs')
+    , sys = require(process.binding('natives').util ? 'util' : 'sys')
+    , server
+	, newfeed = require('./models/newfeed')
+	, newuser = require('./models/user')
+	, RedisStore = require('connect-redis'), multi
+	, local = http.createClient(8080, '127.0.0.1');
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyDecoder());
+  app.use(express.cookieDecoder());
+  app.use(express.session({ store: new RedisStore }));
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.staticProvider(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+function loadUser(req, res, next) {
+  if (req.session.user_id) {
+	next();
+	}
+  else {
+    res.redirect('/new-user');
+  }
+}
+
+// Routes
+
+app.get('/', function(req, res){
+	multi = client.multi();
+	client.zrevrangebyscore('frontPage',1271851245, 1271851241, "limit", "0", "2", function(err, data){
+		if(err){console.log(err)}
+		for (d in data)
+		{
+			multi.hgetall(data[d], function(err, contents){
+			})
+		}
+		multi.exec(function(err, reply){
+			if(err){console.log(err)}
+			console.log(reply);
+			articles = reply;
+			res.render('index', {
+				locals: {title: "Redis", articles: articles}
+			})
+		})
+	})
+});
+
+app.get('/admin', function(req, res){
+	client.lrange('channels', 0, -1, function(err, data){
+		res.render('admin', {
+			locals: {title: "Admin", channels: data}
+		})
+	})
+});
+
+app.post('/admin', function(req, res){
+	channel = req.body.channel;
+	client.lpush('channels', channel, function(err, body){
+		if (err){console.log(err)};
+		res.redirect('/admin');
+	})
+});
+
+app.post('/delete', function(req, res){
+	channel = req.body.channel;
+	console.log(channel);
+	client.lrem('channels', 0, channel, function(err, body){
+		if (err){console.log(err)};
+		res.redirect('/admin');
+	})
+});
+
+app.get("/chat.html", function(req, res){
+var path = url.parse(req.url).pathname;
       fs.readFile(__dirname + path, function(err, data){
         if (err) return send404(res);
         res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
         res.write(data, 'utf8');
         res.end();
       });
-      break;
-      
-    default: send404(res);
-  }
 }),
 
 send404 = function(res){
@@ -39,25 +117,94 @@ send404 = function(res){
   res.end();
 };
 
-server.listen(8080);
-
-// socket.io, I choose you
-// simplest chat application evar
-var io = io.listen(server)
-  , buffer = [];
-  
-io.on('connection', function(client){
-  client.send({ buffer: buffer });
-  client.broadcast({ announcement: client.sessionId + ' connected' });
-  
-  client.on('message', function(message){
-    var msg = { message: [client.sessionId, message] };
-    buffer.push(msg);
-    if (buffer.length > 15) buffer.shift();
-    client.broadcast(msg);
-  });
-
-  client.on('disconnect', function(){
-    client.broadcast({ announcement: client.sessionId + ' disconnected' });
-  });
+app.post('/new', function(req, res){
+	newfeed.fURL(req.body.url, req.body.fname);
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('hello');
 });
+
+app.get('/new-user', function(res, res){
+	res.render('new-user', {
+		locals: {title: "create user"}
+	})
+})
+
+app.post('/new-user', function(req, res){
+	newuser.fUSR(req.body.email, req.body.password);
+    req.session.user_id = req.body.email;
+    res.redirect('/');
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('hello');
+});
+
+app.post('/new/:channel/:feed/:feedName', function(req, res){
+  feedURL = decodeURIComponent(req.params.feed);
+  feedName = decodeURIComponent(req.params.feedName);
+  channel = req.params.channel;
+	client.zadd(feedName, -1, feedURL);	
+	client.rpush(channel, feedName);
+	res.writeHead('200');
+	res.end();
+// TODO createClient()
+});
+
+app.post('/feed/:channel/:feedName', function(req, res){
+	challenge = req.body.challenge;
+	res.writeHead('200');
+	res.write(challenge);
+	req.setEncoding('utf8');
+	feedName = decodeURIComponent(req.params.feedName);
+	channel = req.params.channel;
+	req.on('data', function(data){
+		var d = JSON.parse(data);
+		var dl = d.items.length;
+
+		for (x = 0; x < dl; ++x){
+			picture = "Set Me to some kind of default picture";
+			var content;	
+			if (d.items[x].standardLinks){
+				picture = d.items[x].standardLinks.picture[0].href
+			};
+			console.log(d.title);
+			client.zadd(d.title, d.items[x].postedTime, d.items[x].title, function(err, reply){if (err){console.log(err)}});
+			client.hmset(d.items[x].title, 
+				{
+					"content": d.items[x].content,
+					"link": d.items[x].permalinkUrl,
+					"title": d.items[x].title,
+					"pic": picture,
+					"id": d.title,
+					"channel": channel // NOTE: just added this
+				}, function(err, reply){if (err){console.log("error: " + err)}})
+		};
+	res.redirect('/admin')
+	});
+});
+// Only listen on $ node app.js
+
+if (!module.parent) {
+  app.listen(80);
+  console.log("Express server listening on port %d", app.address().port)
+}
+var into = new function(){
+	var repo = new Array();
+	multi = client.multi();
+	client.lrange('channels', 0, -1, function (err, repo){
+		repo = repo;
+		console.log(repo);
+		for (r in repo)
+		{
+			multi.lrange(repo[r], -0, -1, function (err, reply){})		
+		}
+		multi.exec(function(err, echo){
+			num = echo.length;
+			// need to add min/max to zunionstore to only "recent" scores
+			// or else use limit offset above, depenidng on size of indexes
+			client.zunionstore(['frontPage', num].concat(echo), function (err, front){
+				if(err){console.log(err)};
+				front = front;
+				console.log(front)
+			})
+		});	
+	});
+};
