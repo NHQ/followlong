7f4c21949485a332db31b6472b9fba2762c60c22
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-17 13:18:23 -0800

    april 9

--- server.js
+++ server.js
@@ -56,17 +56,6 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-/*
-function isAdmin(req, res, next) {
-  if (req.session.user_id) {
-	next();
-	}
-  else {
-    res.redirect('/new-user');
-  }
-}
-*/
-
 function getSesh (req, res, next){
 	console.log(req.session.uid);
 	if(!req.session.uid)
@@ -78,37 +67,6 @@ function getSesh (req, res, next){
 		next();
 	}
 };
-
-function userInterface (id){
-	var allem = new Array(),
-	multi = client.multi();
-	client.get(id+':'+channels, function(err, repo){
-		if (err) {console.log(err)}
-		repost = JSON.parse(repo);
-		for (r in repost)
-		{
-			multi.smembers(repost[r])
-		}
-		multi.exec(function (err, echo){
-			console.log(echo+"wgaaaaa");
-			allem = allem.concat.apply(allem, echo);
-			for (a in allem)
-			{
-				multi.zrevrangebyscore(allem[a], epoch(), epoch()-450061, "limit", "0", "20")
-			}
-				multi.exec(function (err, list){
-					if (err){console.log(err)}
-					for (l in list)
-					{
-						multi.hmget(list[l], "title")
-					}
-					multi.exec(function (err, whatThisIs){
-						return whatThisIs;
-					})
-				})
-			})
-		})
-	};
 
 function frontis(facts){
 	var facts = facts;
@@ -156,149 +114,95 @@ app.get('/link', function (req, res ){
 		res.redirect(link);
 		res.end()
 	})
-})
-
-app.get('/userChannels', getSesh, function (req, res){
-		res.writeHead('200');
-		client.get(req.facts+':channels', function (err, channels){
-			if(err){console.log(err)}
-			res.write(channels)
-			res.end();
-		})
 });
 
-app.get('/infoChan', getSesh, function (req, res){
+app.get('/init', getSesh, function (req, res){
 	res.writeHead('200');
-	client.smembers(req.facts+':'+req.query.chan, function (err, info){
-		var multi = client.multi();
-		for (i in info)
-		{
-			multi.zrangebyscore(info[i], -2, -1)
-		}
-		multi.exec(function (err, rayRay){
-			res.write(JSON.stringify(rayRay));
-			console.log(JSON.stringify(rayRay));
-			res.end();
-		})
-	})
-});
-
-app.get('/infoFeed', function (req, res){
-	res.writeHead('200');
-	client.zrevrangebyscore(decodeURIComponent(req.query.feed), epoch(), epoch()-(450061*3), "limit", "0", "75", 'withscores', function(err, info){
-		res.write(JSON.stringify(info));
-		console.log(JSON.stringify(info));
+	client.hgetall(req.facts+'@feeds', function (err, obj){
+		res.write(JSON.stringify(obj));
 		res.end();
 	})
 });
 
-app.get('/userStations', getSesh, function (req, res){
-	console.log(req.query.channel);
-	res.writeHead('200');
-	client.smembers(req.facts+':'+req.query.channel, function (err, sources){
-		multi = client.multi();
-		for (s in sources)
-		{
-		multi.zrevrangebyscore(sources[s], epoch(), epoch()-450061, "limit", "0", "75", 'withscores')
-		}
-		multi.exec(function(err, media){
-			obj = new Object();
-			obj.channel = req.query.channel;
-			obj.articles = media[0];
-			res.write(JSON.stringify(obj));
+app.get('/userChannels', getSesh, function (req, res){
+		res.writeHead('200');
+		client.smembers(req.facts+'@channels', function (err, channels){
+			if(err){console.log(err)}
+			res.write(JSON.stringify(channels))
 			res.end();
-			console.log(obj)
+		})
+});
+
+app.post('/newChannel', getSesh, function (req, res) {
+	res.writeHead('200');
+	client.sadd(req.facts+'@channels', req.query.channel, function(err, result){
+		if (err){res.write('error');res.end()}
+		res.write(JONS.stingify(result))
+		res.end()
+	})
+});
+
+app.post('/deleteChannel', getSesh, function (req, res){
+	res.writeHead('200');
+	client.srem(req.facts+'@channels', req.query.channel, function (err, result){
+		if (err)(res.write("error");res.end())
+	})
+});
+
+app.post('/followFeed', getSesh, function (req, res){
+	res.writeHead('200');
+	client.hset(req.facts+'@feeds', decodeURIComponent(req.query.feed), decodeURIComponent(req.query.channels), function (err, result){
+		if (err){res.write('error');res.end()}
+		res.write('result');
+		res.end();
+		follow(req.query.feed);
+	})
+});
+
+app.post('/followNot', getSesh, function (req, res){
+	res.writeHead('200');
+	client.hdel(req.facts+'@feeds', decodeURIComponent(req.query.feed), function (err, result){
+		if (err){res.write('error');res.end()}
+		res.write('Feed Followed Not');
+		res.end();
+		followNot(req.query.feed)
+	})
+});
+
+app.post('/addChannel', getSesh, function (req, res){
+	res.writeHead('200');
+	client.hget(req.facts+'@feeds', decodeURIComponent(req.query.feed), function (err, json){
+		if (err){res.write('error');res.end()}
+		var channels = JSON.parse(json);
+		channels.push(req.query.channel);
+		client.hset(req.facts+'@feeds', decodeURIComponent(req.query.feed), JSON.stringify(channels), function (err, okay){
+			if (err){res.write('there was an error');res.end()}
+			if (okay){res.write(okay);res.end()}
 		})
 	})
 });
 
-app.get('/userArticles', getSesh, function (req, res){
+app.get('/getFeed', getSesh, function (req. res){
 	res.writeHead('200');
-	source = req.query.station;
-	client.zrevrangebyscore(source, epoch(), epoch()-450061, "limit", "0", "75", function(err, titles){
-		multi = client.multi();
-		for (t in titles)
+	client.zrevrangebyscore(decodeURIComponent(req.query.feed), epoch(), epoch()-450061, "limit", "0", "75", function(err, titles){
+		if(err){res.write('error')}
+		else
 		{
-			multi.hmget(titles[t], 'title', 'score', 'feed', 'link')
-		};
-		multi.exec(function (err, content){
-			res.write(JSON.stringify(content));
-			console.log(JSON.stringify(content));
-			res.end()
-		})
+			var multi = client.multi();
+			for (t in titles)
+			{
+				multi.hmget(title[t], 'title', 'feed', 'score')
+			}
+			mulit.exec(function (err, arrayRay){
+				res.write(JSON.stringify(arrayRay));
+				res.end();
+			})
+		}
 	})
 });
 
 app.get('/', getSesh, function(req, res){
-	var channel = new Array();
-	var articles = new Array();
-	client.get(req.facts+':channels', function (err, channels){
-		if(err){console.log(err)}
-		channels = JSON.parse(channels);
-		for (c in channels)
-		{
-			client.smembers(req.facts+':'+channels[c], function (err, source){
-				if(err){console.log(err)}
-				for (s in source)
-				{
-					client.zrevrangebyscore(source[s], epoch(), epoch()-450061, "limit", "0", "75", function(err, title){
-						if(err){console.log(err)}
-						for (t in title)
-						{
-							client.hmget(title[t], 'title', 'score', 'feed', 'link', function (err, content){
-								if(err){console.log(err)}	
-								media = {'channel':channels[c],'feed':source[s],'content':content};
-								articles.push(media);
-							})
-						}
-					})
-				}
-			})		
-		}
-	})
-	console.log(articles);
-	res.render('index', {
-		locals: {title: "MOSTMODERNIST", admin:0, articles: articles}
-	});
-	res.end();
-});
 
-
-app.get('/user', getSesh, function (req,res){
-	var channels = [], facts;
-	function neon(obj){if (typeof obj === 'string'){channels.push(obj)} else otro(obj)};
-	function otro(obj){channels.push(obj.channel);for (x in obj.subChannels){neon(obj.subChannels[x])}}
-	she = req.facts;
-	client.hgetall(she, function (err, dossier){
-		if(err){console.log(err)}
-		facts = dossier;
-		console.log(facts)
-	});
-	client.get(she+':channels', function (err, string){
-		if(err){console.log(err)}
-		floss = JSON.parse(string);
-		console.log(floss);
-		for (x in floss)
-		{
-			neon(floss[x])
-		}
-		res.render('user', {
-			locals: {title: facts.name, channels: channels, person: facts}
-		})
-		res.end();
-	})
-});
-
-app.get('/info', getSesh, function (req, res){
-	she = req.facts;
-	console.log(she);
-	client.get(she+':channels', function (err, string){
-		if(err){console.log(err)}
-		res.writeHead(200);
-		console.log(string);
-		res.write(string, 'utf8');
-		res.end();
-	})
 });
 
 app.get("/index", getSesh, function(req, res){
@@ -309,255 +213,8 @@ app.get("/index", getSesh, function(req, res){
 		res.end();
       });
 });
-/*
-app.post('/new/channel', getSesh, function (req, res){
-	var newChannel = req.body.channel;
-	client.get(req.facts+':channels', function (err, json){
-		channels = JSON.parse(json);
-		channels.push(newChannel);
-		client.set(req.facts+':channels', JSON.stringify(channels), function(){
-			res.redirect('/index');
-			res.end();
-		})
-	});
-});
-*/
-app.post('/new/channel', getSesh, function (req, res){
-	furl = '';
-	if (req.query.furl)
-	{
-		furl = '#'+req.query.furl
-	}
-	if (req.body.channel === "")
-	{
-		res.redirect('/index');
-		res.end();
-	}
-	else {
-	client.get(req.facts+':channels', function (err, json){
-	 function find(pos, arr) {
-		 var i = pos.shift();  
-	      if (pos.length) { 
-	        return find(pos, arr[i]); 
-	      } else { 
-	        if (typeof arr[i] === 'string')
-				{arr[i] = [arr[i], req.body.channel]}
-			else if (arr[i] === undefined)
-				{arr.push(req.body.channel)}
-			else 
-				{arr[i].push(req.body.channel)}
-	      } 
-	    }
-		channels = JSON.parse(json);
-		index = req.body.station.match(/\d/g);
-		for (i in index){ index.splice(i,1,parseInt(index[i])) }
-		find(index,channels)
-		client.set(req.facts+':channels', JSON.stringify(channels), function(){
-			res.redirect('/index'+furl);
-			res.end();
-		})
-	})}
-});
-app.post('/delete/station', getSesh, function (req,res){
-	client.get(req.facts+':channels', function (err, json){
-	function dulute(pos, arr) {  
-	      if (pos.length > 1) {
-		 var i = pos.shift();
-	        return dulute(pos, arr[i]); 
-	      } 
-			else { 
-				station = arr.splice(pos[0],1);
-				return station;
-				}
-			}
-		channels = JSON.parse(json);
-		index = req.body.station.match(/\d/g);
-		console.log(index);
-		for (i in index){ index.splice(i,1,parseInt(index[i])) }
-		dulute(index,channels);
-		client.set(req.facts+':channels', JSON.stringify(channels), function(){
-			if(err){console.log(err)};
-			client.smembers(req.facts+':'+station, function(err, each){
-				for (every in each)
-				{
-					unfollow(each[every])
-				}
-				client.del(req.facts+':'+station, function(err, done){})					
-				});
-			})
-			res.redirect('/index');
-			res.end();
-		})
-	});
 
 /*
-app.get('/ajax', function (req, res){
-	res.render('ajax', {
-		locals: {title: "MOSTMODERNIST"}
-	})
-});
-
-app.get('/load', function (req, res){
-	res.writeHead(200, {'Content-Type': 'application/json'})
-	multi = client.multi();
-	var jbody = '';
-	var jvar = [];
-	channel = req.query.channel;
-	score = req.query.score;
-	console.log(req.headers);
-	console.log(url.parse(req.url).href+'\n'+channel+'\n'+score);
-	
-	ee.on('godot', function() {
-		console.log('he"s here!')
-	    multi.exec(function(err, nope){
-			if(err){console.log(err)}
-			j = 0;
-			if (j < jvar.length)
-			{
-				client.hmget(jvar[j],'title','score','link','channel','furl', function(err, rere){
-					data = JSON.stringify(rere);
-					res.write(data, 'utf8');
-				})
-				j += 1
-			}
-			else
-			console.log(jbody);
-	        res.end()
-		});
-	});
-	client.smembers(channel, function(err, list){
-		console.log(list);
-		var l = 0;
-		if (l < list.length)
-		{
-			multi.zrevrangebyscore(list[l], score-100, score-90061, function(err, re){
-				for (r in re)
-				jvar.push(re[r])
-			})
-			l += 1
-		}
-		else if (l = list.length)
-		 {ee.emit('godot');
-		client.quit();}
-	})
-});
-*/
-app.get('/admin', function(req, res){
-	var obj = new Object();
-	var cats = [];
-	multi = client.multi();
-	client.smembers('channels', function (err, repo){
-		repo = repo;
-		for (x in repo)
-		{
-			cats.push(repo[x]);
-			multi.smembers(repo[x], function (err, reply){
-			})		
-		}
-		client.quit();
-		multi.exec(function(err, list){
-			for (x = 0; x < list.length; ++x)
-			{
-				obj[cats[x]] = list[x]
-			}
-		res.render('admin', {
-				locals: {title: "admin", go: '/delete/feed', channels: obj }
-			})
-		})
-	});
-	res.end();
-});
-
-app.post('/delete/feed', function (req, res){
-	channel = req.body.channel;
-	feed = decodeURIComponent(req.body.feed);
-	delFeed(channel, feed);
-	res.redirect('/admin');
-	res.end();
-})
-
-app.get('/delete/item/:furl/:item', getSesh, function (req, res){
-	furl = decodeURIComponent(req.params.furl);
-	feed = decodeURIComponent(req.params.item);
-	item = feed.replace(/\s/g, "_")
-	console.log("Feed= "+feed+"\nitem= "+item)
-	client.del(item);
-	client.zrem(furl, item, function(err, res){
-		frontis();
-	});
-	res.redirect('/');
-	res.end();
-})
-
-app.get('/edit', function(req, res){
-	var feed = req.query.feed;
-	var channels = {};
-	client.zrevrangebyscore(feed, epoch(), 0, function(err, items){
-		channels[feed] = items;
-		res.render('admin', 
-		{
-			locals: {title: feed, go: '/delete/item', channels: channels }
-		})
-		client.quit()
-	})
-	res.end();
-})
-
-function delFeed (channel, feed){
-	multi = client.multi();
-	client.zrange(feed, 0, -1, function (err, range){
-		for (r in range)
-		{
-			multi.del(range[r])
-		}
-		multi.exec()
-	});
-	client.del(feed);
-	client.srem(channel, feed);
-	client.srem('allFeeds', feed, function(err, re){
-		client.quit()
-	});
-	unsubscribe(channel, feed);
-	frontis();
-	//perhaps only unsubscribe rather than delete all old ones? Move them out of their parent channel and into the "archives"
-	// after you change "channels" to sets, rather than lists, add srem function to this to delete feed from channel set
-};
-
-function delItem (feed, item) {
-	client.zrem(feed, item);
-	client.del(item);
-};
-
-app.get('/admin/channels', function(req, res){
-	client.lrange('channels', 0, -1, function(err, data){
-		res.body = data;
-		res.end();
-		client.quit();
-	})
-});
-
-app.post('/admin', function(req, res){
-	channel = req.body.channel;
-	client.sadd('channels', channel, function(err, body){
-		if (err){sys.puts(err)};
-		res.redirect('/admin');
-		res.end();
-		client.quit()
-	})
-});
-/*
-app.post('/delete', function(req, res){
-	channel = req.body.channel;
-	sys.puts(channel);
-	client.srem('channels', function(err, body){
-		if (err){sys.puts(err)};
-		res.redirect('/admin');
-		res.end();
-		client.quit()
-	})
-});
-
-
 app.get("/chat.html", function(req, res){
 var path = url.parse(req.url).pathname;
       fs.readFile(__dirname + path, function(err, data){
@@ -567,7 +224,7 @@ var path = url.parse(req.url).pathname;
         res.end();
       });
 }),
-*/
+
 app.get("/schema.json", function(req, res){
 var path = url.parse(req.url).pathname;
       fs.readFile(__dirname + path, function(err, data){
@@ -577,7 +234,7 @@ var path = url.parse(req.url).pathname;
         res.end();
       });
 }),
-
+*/
 app.error(function(err, req, res, next) {
   if (err instanceof NotFound) {
     res.redirect('/');
@@ -590,38 +247,34 @@ send404 = function(res){
 	res.redirect('/');
   res.end();
 };
-/*
-app.get('/new-user', function(req, res){
-	console.log(req.session.user_id);
-	res.render('new-user', {
-		locals: {title: "create user", action: "/new-user"}
-	})
-});
 
-app.post('/new-user', function(req, res){
-	id = (Math.round((new Date().valueOf() * Math.random())) + '');
-	req.session.user_id = req.body.email;
-	newuser.fUSR(req.body.email, req.body.password, id);
-    res.redirect('/');
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('hello');
-});
-/*
-app.get('/test', function(req, res){
-	d = fs.readFileSync('./more.json', 'utf8');
-	datum = JSON.stringify(d);
-	data = JSON.parse(d);
-	var request = local.request('POST', '/feed/vimeo/Angeline_Likes', {
-		'host': 'mostmodernist.no.de',
-		'Application-type': 'application/json'
+function follow(feed){
+	var unfurl = decodeURIComponent(feed);
+	console.log(unfurl);  
+	client.exists(unfurl, function(err,answer){
+		if (err){console.log(err)}
+		if (answer === 0)
+			{
+				client.zadd(unfurl, -1, unfurl);
+				client.incr('subs@'+unfurl);
+				subscribe(unfurl);
+			}
+		else
+		{
+			client.incr('subs@'+unfurl);
+		}			
 	});
-	request.end(d, encoding='utf8');
-	res.redirect('/');
-});
-*/
-
-function unfollow (unfurl){
-	client.decr('subs@'+unfurl, function(err, score){if (score === 0){
+	client.sismember('allfeeds1123848451', unfurl, function(err, answer){
+		if (err){console.log(err)}
+		if (answer === 0)
+		client.sadd('allfeeds1123848451', unfurl);
+	});
+}
+function followNot (furl){
+	var unfurl = decodeURIComponent(furl);
+	client.decr('subs@'+unfurl, function(err, score){
+		if (score === 0){
+		client.del(unfurl);
 		unsubscribe(unfurl)
 	}});
 }
@@ -707,27 +360,8 @@ function retrieve (channel, feed){
 		});
 	});
 };
-*/
 
 app.post('/follow/', getSesh, function(req, res){
-	//path = url.parse(req.url).query;
-	//queriesPls = querystring.parse(path, sep='&', eq='=');
-	var unfurl = decodeURIComponent(req.query.furl);
-	var channelName = req.body.channel;
-	console.log(unfurl);  
-	client.exists(unfurl, function(err,answer){
-		if (err){console.log(err)}
-		if (answer === 0)
-			{
-				client.zadd(unfurl, -1, unfurl);
-				client.incr('subs@'+unfurl);
-			}
-		else
-		{
-			client.incr('subs@'+unfurl);
-		}
-			
-	});	
 	client.sadd(req.facts+':'+channelName, unfurl, function(err, elks){
 			if (err){console.log(err)}
 	});
@@ -742,6 +376,7 @@ app.post('/follow/', getSesh, function(req, res){
 	res.end();	
 	//var retr = setTimeout(function(){retrieve(channel,unfurl)}, 30000);
 });
+*/
 
 app.get('/feed', function(req, res){
 	res.writeHead('200');
@@ -859,8 +494,9 @@ app.get('/auth', function (req, res) {
 								res.end();
 							});
 							client.rpush("everybodyInTheSystem", resulting.id)
-							var channels = '["Culture","Business","Poiltics"]';
-							client.set(resulting.id+':channels', channels)
+							var channels = '["Culture","Business","Politics"]';
+							var placeHolder = "[]"
+							client.hmset(resulting.id+':channels', channels[0],placeHolder,channels[1],placeHolder,channels[2],placeHolder)
 						}						
 					})
 				})
