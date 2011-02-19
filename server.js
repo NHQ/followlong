4547eb12a3b9b74c019665f9ceab1831925c92fd
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-18 16:00:22 -0800

    woikn

--- server.js
+++ server.js
@@ -198,7 +198,7 @@ function subscribe (channel, feed){
 function retrieve (channel, feed){
 	var spfdr = http.createClient(80, 'superfeedr.com');
 	var ditto = new String();
-	data = "hub.mode=retrieve&hub.topic="+feed+"&hub.callback=http://64.30.138.240/feed/?channel="+channel+"&furl="+encodeURIComponent(feed);
+	data = "hub.mode=retrieve&hub.topic="+feed+"&hub.callback=http://64.30.138.240/feed/?channel="+channel+"&furl="+feed;
 	var request = spfdr.request('GET', '/hubbub', {
 		'Host':'superfeedr.com',
 		"Authorization":"basic TkhROmxvb3Bob2xl",
