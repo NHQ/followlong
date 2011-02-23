ecff9c543b3c43af86cf432a85a86fa643ead91a
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-23 15:48:24 -0800

    wednesday

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -4,28 +4,14 @@ $(function(){
 
   $container.isotope({
     itemSelector : '.articles',
-    getSortData : {
-      symbol : function( $elem ) {
-        return $elem.attr('data-category');
-      }
-	  score : function( $elem ) {
-		return $elem.attr('data-score')
-	  }
-    },
   });
 
-  // sorting
-  $('#sort a').click(function(){
-    // get href attribute, minus the #
-    var $this = $(this),
-        sortName = $this.attr('href').slice(1),
-        //asc = $this.parents('.sort').hasClass('asc');
-    $container.isotope({ 
-      sortBy : symbol,
-      //sortAscending : asc
-    });
-    return false;
-  });
+  // filter
+$('#filter a').click(function(){
+  var selector = $(this).attr('data-filter');
+  $('#container').isotope({ filter: selector });
+  return false;
+});
 
 
 });
