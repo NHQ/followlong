7c55867d3aa97b63bbc1d48a1c8f42f2274ef748
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-26 16:07:03 -0800

    friday

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -18,7 +18,7 @@ $('#filter a').click(function(){
   return false;
 });
 
-$('div.articles').bind('click', function(event){
+$('div.articles').live('click', function(event){
 	$(this).find('div.summary').css('display', 'block');
 });
 
