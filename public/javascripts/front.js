1aae030208ab2b73ff0df6beba8ede101068150f
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-28 16:41:19 -0800

    monday

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -29,7 +29,7 @@ $(window).load(function(){
 		var append;
 		for (i in data)
 		{
-			append += '<div class="articles '+data[i][3]+'", data-score='+data[i][1]+', data-category='+data[i][3]+', id='+data[i][4]+'><h2><a href='+data[i][2]+', class="title">'+data[i][0]+'</a></h2></div>'
+			append += '<div class="articles '+data[i][3]+'", data-score='+data[i][1]+', data-category='+data[i][3]+', id='+data[i][4]+'><h2><a href='data[i][2]', class="title">'+data[i][0]+'</a></h2></div>'
 		}
 		$('#container').append(append);
 	})
