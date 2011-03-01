bf2268ce5a04d3ca8d42909d251ac27bbc8ccc10
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-28 21:45:08 -0800

    monday

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -21,7 +21,7 @@ $('#filter a').click(function(){
   var selector = $(this).attr('data-filter');
   $('#container').isotope({ filter: selector });
 	var dingo = '';
-	$('#container .articles').find(selector).each(function(){
+	$('#container').find('.articles '+selector).each(function(){
 		dingo += $(this).attr('data-score')
 	});
 	alert(dingo);
