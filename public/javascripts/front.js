6fb515a08c5b4bd105322c536407fd3a0586aa18
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-28 18:02:19 -0800

    monday

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -30,12 +30,11 @@ $(document).ready(function(){
 	    .success(function(data) {
 		append= '';
 		for (i in data)
-		{	
-		$append = $('<div class="articles '+data[i][3]+'", data-score="'+data[i][1]+'", data-category="'+data[i][3]+'", id="'+data[i][4]+'"><div class="fit"><h2><a href="'+data[i][2]+'",class="title">'+data[i][0]+'</a></h2></div></div>')
-		$container.isotope('insert', $append)
+		{
+			append += '<div class="articles '+data[i][3]+'", data-score="'+data[i][1]+'", data-category="'+data[i][3]+'", id="'+data[i][4]+'"><div class="fit"><h2><a href="'+data[i][2]+'",class="title">'+data[i][0]+'</a></h2></div></div>'
 		}
-		//var $append = $( append )
-		//$container.isotope('insert', $append);
+		var $append = $( append )
+		$container.isotope('insert', $append);
 	})
 	    .error(function() { alert("error: "+textStatus); })
 });
