40a1cb6860a4f3c69b6430a51f815a270beece8c
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-28 23:38:57 -0800

    monday

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -32,7 +32,7 @@ $('#filter a').click(function(){
 
 function loaded(score, selector){
 	channel = selector.slice(1);
-	url = "/load/"+channel+"/"+score;
+	url = "./load/"+channel+"/"+score;
 	alert(url);
 	var jqxhr = $.ajax({ url: url, dataType: "json"})
 	    .success(function(data) {
