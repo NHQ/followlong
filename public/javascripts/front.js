1ce151022d3438c06814c3302b02be8771a59b57
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-28 17:21:55 -0800

    monday

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -34,7 +34,11 @@ $(document).ready(function(){
 			append += '<div class="articles '+data[i][3]+'", data-score="'+data[i][1]+'", data-category="'+data[i][3]+'", id="'+data[i][4]+'"><div class="fit"><h2><a href="'+data[i][2]+'",class="title">'+data[i][0]+'</a></h2></div></div>'
 		}
 		var $append = $( append )
-		$container.isotope('insert', $append);
+		$container.isotope({animationOptions: {
+	     duration: 100,
+	     easing: 'linear',
+	     queue: false
+	   }}).isotope('insert', $append);
 	})
 	    .error(function() { alert("error: "+textStatus); })
 });
