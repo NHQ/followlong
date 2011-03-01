e145aa7bd7cd46738a923d6092cf3f1882d95c69
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-02-28 20:47:13 -0800

    monday

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -36,11 +36,12 @@ $(document).ready(function(){
 	})
 	    .error(function() { alert("error: "+textStatus); })
 });
-Path.map('/#/culture')to.(function(){
-	$('#container').isotope({ filter: '.culture' });
-  return false;
-})
+
 $(document).ready(function()}{
+	Path.map('/#/culture')to.(function(){
+		$('#container').isotope({ filter: '.culture' });
+	  return false;
+	})
 Path.listen();	
 })
 });
