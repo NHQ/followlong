56f489c8b543eb083c159c69370ed13fa0eea9f4
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-03-04 14:08:45 -0800

    thurs

--- server.js
+++ server.js
@@ -583,7 +583,7 @@ app.get('/fb', function (req, res) {
 
 app.get('/auth', function (req, res) {
 	code = req.query.code;
-	url = '/oauth/access_token?client_id=190292354344532&redirect_uri=http%3A%2F%2Fmostmodernist.no.de%3A80%2Fauth&client_secret=6a8433e613782515148f6b2ee038cb1a&code='+code
+	url = 'oauth/access_token?client_id=190292354344532&redirect_uri=http%3A%2F%2Fmostmodernist.no.de%3A80%2Fauth&client_secret=6a8433e613782515148f6b2ee038cb1a&code='+code
 	var fbGetAccessToken = http.createClient('443', 'https://graph.facebook.com/', secure=true);
 	request = fbGetAccessToken.request('GET', url, {
 		'Host':'graph.facebook.com',
