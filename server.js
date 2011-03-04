3ba676b090102acb144752b12d5fdc8d9f996a18
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-03-03 20:31:56 -0800

    thurs

--- server.js
+++ server.js
@@ -581,7 +581,7 @@ app.get('/fb', function (req, res) {
 });
 
 app.get('/auth', function (req, res) {
-  facebookClient.getAccessToken({redirect_uri: 'http://mostmodernist.no.de/auth', code: req.param('code')}, function (error, token) {
+  facebookClient.getAccessToken({redirect_uri: 'http://mostmodernist.no.de/auth', code: req.query.code}, function (error, token) {
 	if(error){console.log(error);}
     res.render('client', {
       locals: {
