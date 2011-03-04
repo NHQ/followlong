2cc36cc11c7d0d3f0954ab2edc24a8dd30174deb
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-03-04 14:33:25 -0800

    thurs

--- server.js
+++ server.js
@@ -587,7 +587,7 @@ console.log(code);
 res.writeHead('200');
 res.end();
 url = '/oauth/access_token?client_id=190292354344532&redirect_uri=http%3A%2F%2Fmostmodernist.no.de%3A80%2Fauth&client_secret=6a8433e613782515148f6b2ee038cb1a&code='+code;
-var fbGetAccessToken = http.createClient('443', 'graph.facebook.com', secure=true);
+var fbGetAccessToken = http.createClient('443', 'https://graph.facebook.com', secure=true);
 request = fbGetAccessToken.request('GET', url, {
 	'Host':'facebook.com',
 	'Content-Length': 0
