adfa04fb3a792f5b12240c9b809edd0f80465c17
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-03-08 20:29:09 -0800

    tuesday

--- server.js
+++ server.js
@@ -171,15 +171,6 @@ app.get('/users2', getSesh, function (req,res){
 		})
 });
 
-app.get("/index", getSesh, function (req,res){
-	var path = url.parse(req.url).pathname;
-	      fs.readFile(__dirname + '/public/HTMLS/index.html', function(err, data){
-	        if (err) return send404(res);
-	        res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'})
-	        res.write(data, 'utf8');
-	        res.end();
-	      });
-});
 
 app.post('/new/channel', getSesh, function (req, res){
 	var newChannel = req.body.channel;
